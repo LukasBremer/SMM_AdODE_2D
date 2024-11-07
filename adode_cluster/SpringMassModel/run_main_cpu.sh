6ef3f34538b01f61ab9f0f7f7674e96fb0138a3e
LukasBremer <l.bremer111@googlemail.com>
2024-11-07 13:12:43 +0100

    synchronized with cluster, ready for merge with main

--- adode_cluster/SpringMassModel/run_main_cpu.sh
+++ adode_cluster/SpringMassModel/run_main_cpu.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH -t 0-00:00:00
+#SBATCH -t 04:00:00
 #SBATCH -p medium
-#SBATCH -o /scratch/users/lbremer/out/job-%A_%a.out
-#SBATCH -e /scratch/users/lbremer/out/job-%A_%a.err
-
-#SBATCH --mem=100G
-#SBATCH -a 1-99
-
-#get every decimal number from the task ID
+#SBATCH -o /scratch/users/lbremer/adode_cluster/out/job-%A_%a.out
+#SBATCH -e /scratch/users/lbremer/adode_cluster/err/job-%A_%a.err
+#SBATCH -C cascadelake
+#SBATCH --mem=10G
+#SBATCH -a 0-99
 
 i=$((SLURM_ARRAY_TASK_ID / 10))
 j=$((SLURM_ARRAY_TASK_ID % 10))
-k=0
+k=5
 
 module load python 
+source ../.venv/bin/activate
 python Main.py $i $j $k
