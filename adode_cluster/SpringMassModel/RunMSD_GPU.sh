fa4801b6ed412f6cd171ba49a55b343aaeb1cb0a
LukasBremer <l.bremer111@googlemail.com>
2025-02-14 16:46:52 +0100

    savety

---
+++ adode_cluster/SpringMassModel/RunMSD_GPU.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=kernik
+#SBATCH --output=/scratch/users/ikottla/Projects/ionic-atrical-model-fit/cluster/out/%j_kernik.out
+#SBATCH --error=/scratch/users/ikottla/Projects/ionic-atrical-model-fit/cluster/error/%j_kernik.err
+#SBATCH --time 0-01:00:00
+#SBATCH --qos 2h
+#SBATCH -p gpu
+#SBATCH -G RTX5000
+#SBATCH --mem 16G
